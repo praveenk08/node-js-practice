ac3214bc01d0186b3e6e336b8721deedd3dc7ef6
Praveen Kumar <pk46066@gmail.com>
2024-11-21 19:29:50 +0530

    http module

--- filesystem.js
+++ filesystem.js
@@ -1,10 +1,16 @@
+const { error } = require('console');
 const fs = require('fs');
 
-const textIn= fs.readFileSync('./input.txt','utf-8'); 
 
-console.log(textIn);
+// const textIn= fs.readFileSync('./input.txt','utf-8'); // Read file Sync 
 
-let contest =`${textIn} Hello i"m PHP and APIs backend developer: `;
+const textInAsync= fs.readFile('./input.txt','utf-8', (error,data)=>{
+  console.log('data '+data);
+}); // Read file Async 
+
+
+// let contest =`${textIn} Hello i"m PHP and APIs backend developer: `;
+let contest =`Hello i"m PHP and APIs backend developer: `;
 const textwrite= fs.writeFileSync('./input.txt', contest); 
 
 console.log(textwrite);
